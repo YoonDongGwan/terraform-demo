1ed5e25dcd42347b298bbb52079c78cf34b5b276
윤동관 <ydg3825@gmail.com>
2024-12-10 17:44:18 +0900

    subnet 모듈화 중

--- aws/subnet/main.tf
+++ aws/subnet/main.tf
@@ -1,9 +1,26 @@
 resource "aws_subnet" "subnet" {
+  count = length(var.subnet_cidr_blocks)
   vpc_id = var.vpc_id
-  cidr_block = var.subnet_cidr_block
-  availability_zone = var.availability_zone
+  cidr_block = element(var.subnet_cidr_blocks,count.index)
+  availability_zone = element(var.availability_zones, count.index)
   map_public_ip_on_launch = var.automatic_public_ip
   tags = {
     Name = var.subnet_name
+  }
+}
+resource "aws_internet_gateway" "internet_gateway" {
+  count = 1
+  vpc_id = var.vpc_id
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = var.vpc_id
+  route {
+    cidr_block = var.route_table_cidr_block
+    gateway_id = var.route_table_gateway_id
+  }
+  route {
+    cidr_block = var.vpc_cidr_block
+    gateway_id = "local"
   }
 }
--- aws/subnet/variable.tf
+++ aws/subnet/variable.tf
@@ -1,15 +1,24 @@
 variable "vpc_id" {
   type = string
 }
-variable "subnet_cidr_block" {
-  type = string
+variable "subnet_cidr_blocks" {
+  type = list(string)
 }
-variable "availability_zone" {
-  type = string
+variable "availability_zones" {
+  type = list(string)
 }
 variable "subnet_name" {
   type = string
 }
 variable "automatic_public_ip" {
   type = bool
+}
+variable "vpc_cidr_block" {
+  type = string
+}
+variable "route_table_cidr_block" {
+  type = string
+}
+variable "route_table_gateway_id" {
+  type = string
 }
