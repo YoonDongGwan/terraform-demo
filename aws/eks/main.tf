98622229267b5294c503d747d23b8a5b8578c4d9
윤동관 <ydg3825@gmail.com>
2024-12-12 19:16:44 +0900

    eks_cluster assume_role 변경

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -41,8 +41,22 @@ data "aws_iam_policy_document" "eks_assume_role" {
 }
 
 resource "aws_iam_role" "eks_cluster" {
-  name               = "eks-cluster-role"
-  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
+  name = "eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
